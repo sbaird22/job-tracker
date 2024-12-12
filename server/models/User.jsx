982a296ac02d01c4fa4dd6223fa5034fa63653e6
DDeMarois <Drew.DeMarois@du.edu>
2024-12-11 21:03:45 -0700

    added code to applicationController and User

---
+++ server/models/User.jsx
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize';
+
+export default (sequelize) => sequelize.define('User', {
+    userName: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
