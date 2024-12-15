4d741f129b00724215e98e3710eaafc7b7933c5d
sbaird22 <shanebaird30@yahoo.com>
2024-12-15 12:50:10 -0700

    Moved from create react to vite

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/applications', applicationRoutes);
 
-const PORT = process.env.PORT || 5000;
+const apiUrl = import.meta.env.VITE_API_URL;
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
